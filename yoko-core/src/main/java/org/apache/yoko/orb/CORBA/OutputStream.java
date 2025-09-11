4626c50a649dbf143bae11f1281de9819bf4bccc
Joe Chacko <chackoj@uk.ibm.com>
2025-09-11 16:37:51 +0100

    refactor: simplify codeset handling

--- yoko-core/src/main/java/org/apache/yoko/orb/CORBA/OutputStream.java
+++ yoko-core/src/main/java/org/apache/yoko/orb/CORBA/OutputStream.java
@@ -621,19 +621,7 @@ public final class OutputStream extends org.omg.CORBA_2_3.portable.OutputStream 
         final char[] arr = value.toCharArray();
         final CodeConverterBase converter = codeConverters_.outputCharConverter;
 
-        if (!charWriterRequired_) {
-            int len = arr.length;
-            int capacity = len + 1;
-            write_ulong(capacity); // writes the length and ensures a two-byte boundary alignment
-            addCapacity(capacity);
-            if (charConversionRequired_) {
-                for (char c: arr) writeBuffer.writeByte(converter.convert(checkChar(c)));
-            } else {
-                for (char c: arr) writeBuffer.writeByte(checkChar(c));
-            }
-            // write null terminator
-            writeBuffer.writeByte(0);
-        } else {
+        if (charWriterRequired_) { // write one or more bytes per char
             // We don't know how much space each character will require: each char could take up to four bytes.
             // To avoid re-allocation, create a large enough temporary buffer up front.
             // NOTE: we need to use a temporary buffer to count the bytes reliably, because
@@ -653,6 +641,18 @@ public final class OutputStream extends org.omg.CORBA_2_3.portable.OutputStream 
             // and write the contents
             addCapacity(tmpWriter.length());
             tmpWriter.readFromStart().readBytes(writeBuffer);
+        } else { // write one byte per char
+            int len = arr.length;
+            int capacity = len + 1;
+            write_ulong(capacity); // writes the length and ensures a two-byte boundary alignment
+            addCapacity(capacity);
+            if (charConversionRequired_) {
+                for (char c: arr) writeBuffer.writeByte(converter.convert(checkChar(c)));
+            } else {
+                for (char c: arr) writeBuffer.writeByte(checkChar(c));
+            }
+            // write null terminator
+            writeBuffer.writeByte(0);
         }
     }
 
