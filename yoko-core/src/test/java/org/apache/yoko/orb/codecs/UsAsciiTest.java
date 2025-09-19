85dd302eac48ae0f102e256eaa2517b87022e94e
Joe Chacko <chackoj@uk.ibm.com>
2025-09-19 12:32:52 +0100

    test: verify simple codec behaviour

---
+++ yoko-core/src/test/java/org/apache/yoko/orb/codecs/UsAsciiTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an \"AS IS\" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.apache.yoko.orb.codecs;
+
+import org.apache.yoko.io.ReadBuffer;
+import org.apache.yoko.io.WriteBuffer;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.apache.yoko.orb.codecs.SimpleCodec.US_ASCII;
+
+class UsAsciiTest extends AbstractSimpleCodecTest {
+    UsAsciiTest() {
+        super(US_ASCII, WriteBuffer::writeByte, ReadBuffer::readByteAsChar);
+    }
+
+    @ParameterizedTest(name = "Decode and Encode {0} \"{2}\"")
+    @MethodSource("asciiChars")
+    public void testAsciiChars(String hex, int codepoint, char c) {
+        assertValidChar(c);
+    }
+
+    @ParameterizedTest(name = "Decode and Encode {0} \"{2}\"")
+    @MethodSource("isoLatinChars")
+    void testIsoLatinCharsChars(String hex, int codepoint, char c) {
+        assertInvalidChar(c);
+    }
+
+    @ParameterizedTest(name = "Encode {0} \"{2}\"")
+    @MethodSource("wideChars")
+    void testWideChars(String hex, int codepoint, char c) {
+        assertEncodingFails(c);
+    }
+}
