b9620398eabb0b37aa0bd9de3719f2b956ee0dce
Joe Chacko <chackoj@uk.ibm.com>
2025-10-22 10:59:37 +0100

    docs: describe yoko-util's io package

---
+++ yoko-util/src/main/java/org/apache/yoko/io/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2025 IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an \"AS IS\" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * This package provides functionality for reading and writing Java primitive types
+ * from and to byte arrays. Since this is Yoko, the domain is obviously CORBA marshalling.
+ * However, this is a utility in a layer above any CORBA specifics, and deals only with Java
+ * primitive types.
+ */
+package org.apache.yoko.io;
